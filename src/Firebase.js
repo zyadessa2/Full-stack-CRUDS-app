3fda6a50eab24f6462849ea54051f6fe0abe6086
ziad <helmyzez@gmail.com>
2024-03-21 15:13:15 +0200

    create data in firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyD60W9Gj7eWNcr-9Y23NycuxgHuHOFdKZI",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app)
+export const db = getFirestore(app)
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,9 +3,37 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import {addDoc, collection, doc , serverTimestamp, setDoc} from "firebase/firestore"
+import { auth, db } from "../../Firebase";
+import {createUserWithEmailAndPassword} from "firebase/auth" // to auth with email and pass
+
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
+  const [data, setData] = useState({});
+
+  const handleInput =(e)=>{
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({...data, [id]:value})
+  }
+console.log(data);
+  const handleAdd =async (e)=>{
+    e.preventDefault();
+    try{
+      const res = await createUserWithEmailAndPassword(auth , data.email ,data.password)
+      await setDoc(doc(db , "users" ,res.user.uid),{
+        ...data,
+        timeStamp: serverTimestamp(),
+      })
+    }catch(err){
+      console.log(err);
+    }
+      
+      
+  }
+
 
   return (
     <div className="new">
@@ -27,7 +55,7 @@ const New = ({ inputs, title }) => {
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
@@ -43,10 +71,10 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input type={input.type} placeholder={input.placeholder} onChange={handleInput} id={input.id}/>
                 </div>
               ))}
-              <button>Send</button>
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
